29ce1841c239c860f162132ee682119521b4847c
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-06-15 20:17:45 +0000

    Add Book Component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux/es/exports";
+import { deleteBook } from "../redux/books/books";
 
-const Book = ({ title, author }) => (
-  <>
-    <h3>{title}</h3>
-    <p>{author}</p>
-    <button type="button">Remove</button>
-  </>
-);
+const Book = ({ title, author, id }) => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <button onClick={() => dispatch(deleteBook(id))} type="button">
+        Remove
+      </button>
+    </>
+  );
+};
 export default Book;
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
