8277698ed0ed610abb2d2872d1853e649197c8ad
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-06-15 19:53:24 +0000

    Import Hooks from redux

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import {v4 as uuidv4} from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const AddBook = () => (
   <form>
