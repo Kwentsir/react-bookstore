6c989dff86051ff9247e5c0a91e5a46c5c11518d
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-06-15 20:09:47 +0000

    Update addBooks Component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,20 +1,44 @@
-import React, {useState} from 'react';
-import { useDispatch } from 'react-redux/es/exports';
-import {v4 as uuidv4} from 'uuid';
-import { addBook } from '../redux/books/books';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux/es/exports";
+import { v4 as uuidv4 } from "uuid";
+import { addBook } from "../redux/books/books";
 
-const AddBook = () => (
-  <form>
-    <input
-      style={{
-        marginRight: '1rem',
-      }}
+const AddBook = () => {
+  const [form, setForm] = useState({ title: "", author: "" });
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const book = { ...form, id: uuidv4() };
+    dispatch(addBook(book));
+    setForm({ title: "", author: "" });
+  };
+
+  return (
+    <form>
+      <input
+        style={{
+          marginRight: "1rem",
+        }}
+        type="text"
+        name="title"
+        placeholder="Type Book Title Here"
+        value={form.title}
+        onChange={(e) => handleChange(e)}
+      />
+      <input style={{marginRight: '1rem',}}
+      name="author"
       type="text"
-      placeholder="Type Book Title Here"
-    />
-
-    <button type="submit">Add Book</button>
-  </form>
-);
-
+      placeholder="Type Author Name"
+      value={form.author}
+      onChange={(e) => handleChange(e)}
+      />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
 export default AddBook;
